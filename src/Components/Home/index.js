222f48dbe01f729ef4c63a4b429ca03064b6b95c
mallikarjun <mallijangam@gmail.com>
2025-01-28 01:13:59 +0530

    Added Add User functionality

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -15,7 +15,13 @@ class Home extends Component {
     state = {
         usersList: [],
         apiStatus: apiStatuses.initial,
-        errorMsg: ''
+        errorMsg: '',
+        newName: '',
+        newUsername: '',
+        newEmail: '',
+        mailValidMsg: '',
+        newCompany: '',
+        formErrorMsg: ''
     }
 
     componentDidMount() {
@@ -61,22 +67,32 @@ class Home extends Component {
 
     deleteUser = async id => {
         const { usersList } = this.state
-        const url = `https://jsonplaceholder.typicode.com/users/${id}`
-        await fetch(url, { method: 'DELETE' })
-        const filteredUsers = usersList.filter(each => each.id !== id)
-        this.setState({ usersList: filteredUsers })
+        try {
+            const url = `https://jsonplaceholder.typicode.com/users/${id}`
+            await fetch(url, { method: 'DELETE' })
+            const filteredUsers = usersList.filter(each => each.id !== id)
+            this.setState({ usersList: filteredUsers })
+
+        } catch (error) {
+            this.setState({ apiStatus: apiStatuses.failure })
+        }
     }
 
     editUser = async (id, userDetails) => {
         const { usersList } = this.state
-        const url = `https://jsonplaceholder.typicode.com/users/${id}`
-        await fetch(url, {
-            method: 'PUT',
-            body: JSON.stringify(userDetails),
-            headers: {
-                'Content-type': "application/json"
-            }
-        })
+        try {
+            const url = `https://jsonplaceholder.typicode.com/users/${id}`
+            await fetch(url, {
+                method: 'PUT',
+                body: JSON.stringify(userDetails),
+                headers: {
+                    'Content-type': "application/json"
+                }
+            })
+
+        } catch (error) {
+            this.setState({ apiStatus: apiStatuses.failure })
+        }
         const editedUsers = usersList.map(each => {
             if (each.id === id) {
                 return {
@@ -126,6 +142,103 @@ class Home extends Component {
         }
     }
 
+    onChangeNewName = event => {
+        this.setState({ newName: event.target.value })
+    }
+    onChangeNewUsername = event => {
+        this.setState({ newUsername: event.target.value })
+    }
+    onChangeNewEmail = event => {
+        const { value } = event.target
+        if (value.endsWith("@gmail.com")) {
+            this.setState({ newEmail: value, mailValidMsg: '' })
+        } else {
+            this.setState({ newEmail: value, mailValidMsg: "Must ends with @gmail.com" })
+        }
+    }
+    onChangeNewCompany = event => {
+        this.setState({ newCompany: event.target.value })
+    }
+
+    onSubmitForm = async event => {
+        event.preventDefault()
+        const { newName, newUsername, newEmail, newCompany, mailValidMsg } = this.state
+        if (newName !== '' && newUsername !== '' && mailValidMsg === '' && newCompany !== '') {
+            try {
+                const url = 'https://jsonplaceholder.typicode.com/users'
+                const response = await fetch(url, {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        name: newName,
+                        username: newUsername,
+                        email: newEmail,
+                        company: newCompany
+                    }),
+                    headers: {
+                        "Content-type": "application/json"
+                    }
+                })
+                if (response.ok) {
+                    this.setState({errorMsg: ''})
+                    const data = await response.json()
+                    alert(`New User is created successfully. ID:-${data.id}`)
+                } else {
+                    this.setState({ apiStatus: apiStatuses.failure, errorMsg: '' })
+                }
+            } catch (error) {
+                this.setState({ apiStatus: apiStatuses.failure , errorMsg: ''})
+            }
+        }else {
+            this.setState({errorMsg: "Fill all the details while add New User"})
+        }
+
+
+    }
+
+    renderPopup = () => {
+        const { mailValidMsg } = this.state
+        return (
+            <Popup
+                modal
+                trigger={
+                    <div className='add-button-container'>
+                        <button type='button' className='add-button'>Add User</button>
+                    </div>
+                }
+            >
+                {close => (
+                    <form className='add-user-form' onSubmit={(event) => {
+                        this.onSubmitForm(event);
+                        close();
+                    }}>
+                        <h1 className='add-user-heading'>New User</h1>
+                        <div className='add-user-input-container'>
+                            <label htmlFor='NAME' className='add-user-label-text'>NAME</label>
+                            <input id='NAME' type='text' placeholder='Name' className='add-user-input-box' onChange={this.onChangeNewName} />
+                        </div>
+                        <div className='add-user-input-container'>
+                            <label htmlFor='USERNAME' className='add-user-label-text'>USERNAME</label>
+                            <input id='USERNAME' type='text' placeholder='Username' className='add-user-input-box' onChange={this.onChangeNewUsername} />
+                        </div>
+                        <div className='add-user-input-container'>
+                            <label htmlFor='EMAIL' className='add-user-label-text'>EMAIL</label>
+                            <input id='EMAIL' type='text' placeholder='Email' className='add-user-input-box' onChange={this.onChangeNewEmail} />
+                            {mailValidMsg !== '' ? <p className='mail-valid-msg'>{mailValidMsg}</p> : null}
+                        </div>
+                        <div className='add-user-input-container'>
+                            <label htmlFor='COMPANY' className='add-user-label-text'>COMPANY</label>
+                            <input id='COMPANY' type='text' placeholder='Company' className='add-user-input-box' onChange={this.onChangeNewCompany} />
+                        </div>
+                        <div className='add-user-buttons-container'>
+                            <button type='submit' className='add-user-submit-button'>Submit</button>
+                            <button type='button' onClick={close} className='add-user-cancel-button'>Cancel</button>
+                        </div>
+                    </form>
+                )}
+            </Popup>
+        )
+    }
+
     render() {
         const { errorMsg } = this.state
         return (
@@ -134,9 +247,7 @@ class Home extends Component {
                     <h1 className='logo'>A.</h1>
                     <h1 className='company-name'>AJACKUS</h1>
                 </div>
-                <div className='add-button-container'>
-                    <button type='button' className='add-button'>Add User</button>
-                </div>
+                {this.renderPopup()}
                 {errorMsg !== '' ? <p className='error-msg'>*{errorMsg}</p> : null}
                 {this.renderOutput()}
             </div>
